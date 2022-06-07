a5f564925bd3d846e37dae0587ed76439d4d5d15
Victor Kindrat <kindratvictor07@gmail.com>
2022-06-07 18:35:01 +0300

    a little update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function fillMySkills(count) {
     for (let i = 1; i!== count + 1; i++) {
+        $('.main__iKnow-container').append('<div class="main__iKnow-item main__iKnow-item'+i+'"></div>');
         $('.main__iKnow-item' + i).css('background', '#5534A5 url("./images/day/main/iknow/'+i+'.svg") no-repeat center')
     }
 }
